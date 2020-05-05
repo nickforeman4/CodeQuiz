155692729a6a84f2f768a38b07afd075b1fe274c
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-05-04 23:29:55 -0500

    Uploading files

---
+++ script.js
@@ -0,0 +1,17 @@
+///Pointers to DOM Elements
+var startbtn = document.getElementById("startbtn")
+// same for question and options
+
+
+var quizArray = [
+    {
+    question: "What is CSS",
+    answers:["Cascasding Style sheets","",""],
+    right:0
+},
+{
+  
+        question: "What is HTML",
+        answers:["hyper","Hyper text markup language",""],
+        right:1 
+}]
